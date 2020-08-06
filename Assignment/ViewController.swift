863392ba4143c0f6cf57e42a957dbb860d3d693b
Ravi kumar <56251535+imravi0009@users.noreply.github.com>
2020-08-06 21:52:25 +0530

    Initial commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Ravi kumar on 06/08/20.
+//  Copyright © 2020 Ravi kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
